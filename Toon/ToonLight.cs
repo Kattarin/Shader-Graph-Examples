ad618009d6d276d14bfdf949637a312c0aa8d389
Kattarin <40930558+Kattarin@users.noreply.github.com>
2019-04-17 19:53:19 -0700

    Initial check-in.

---
+++ Toon/ToonLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Add script to lighting source
+// Updates toon shadows for toon shader
+
+public class ToonLight : MonoBehaviour
+{
+    private Light light = null;
+
+    private void OnEnable()
+    {
+        light = this.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        Shader.SetGlobalVector("_ToonLightDirection", -this.transform.forward);
+    }
+}
